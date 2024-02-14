7f2ec8f9d70b40177bb2cdfe8716608431bb8457
rafaelcosta <rafaelcosta@brkambiental.com.br>
2024-02-14 16:22:23 -0300

    added to pom.xml the dependency for lombok and configuring anotantion him to entity classes

---
+++ src/main/java/com/rfaeljr/bookstoremanager/entity/Author.java
@@ -0,0 +1,22 @@
+package com.rfaeljr.bookstoremanager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+    @Column(nullable = false)
+    private Integer age;
+}
